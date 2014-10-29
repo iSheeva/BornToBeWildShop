471e6525d2875f03f57221f79e1aa11671238f0a
TL Nguyen <tlnguyen@TLs-Mac.local>
2014-10-29 14:54:27 +0200

    initial project files

---
+++ Wild Shop/Wild Shop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wild Shop
+//
+//  Created by TL Nguyen on 10/29/14.
+//  Copyright (c) 2014 Team Sheeva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
