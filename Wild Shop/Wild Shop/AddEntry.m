dcec0c21e31aae4b025871e2c0a2aa935c8e7243
Archangel <radishikerov@yahoo.com>
2014-11-03 16:45:36 +0300

    Added AddEntry Class for Testing Functionality

---
+++ Wild Shop/Wild Shop/AddEntry.m
@@ -0,0 +1,13 @@
+//
+//  AddEntry.m
+//  Wild Shop
+//
+//  Created by Admin on 11/3/14.
+//  Copyright (c) 2014 Team Sheeva. All rights reserved.
+//
+
+#import "AddEntry.h"
+
+@implementation AddEntry
+
+@end
